b260e9d3943bcd78652ff08018dadcf41e2e1a4a
DevanBansal <d.bansal@mynyte.co.uk>
2018-03-16 12:19:22 +0000

    upload test

---
+++ templates/google-analytics-template.php
@@ -0,0 +1,8 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-69067179-39"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-69067179-39');
+</script>
